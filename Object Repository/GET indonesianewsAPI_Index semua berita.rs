86127d950ee6f9f350c779dc8e4013212f9ae9d9
nositianika_ <nosiitianikka@gmail.com>
2020-12-14 16:22:12 +0700

    commit api

---
+++ Object Repository/GET indonesianewsAPI_Index semua berita.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>GET indonesianewsAPI_Index semua berita</name>
+   <tag></tag>
+   <elementGuidId>3cee879c-bfd5-4b7c-a8e6-b1c9d17b6f4e</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent></httpBodyContent>
+   <httpBodyType></httpBodyType>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>GET</restRequestMethod>
+   <restUrl>https://www.news.developeridn.com/</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceFunction></soapServiceFunction>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
